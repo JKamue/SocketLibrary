205563b85b9fef63e961a04ab54f900e47af0973
JKamue <mail@jkamue.de>
2020-10-25 15:30:06 +0100

    Added Message as IPacket wrapper

---
+++ SocketLibrary/Messages/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocketLibrary.Messages
+{
+    [Serializable]
+    class Message
+    {
+        public Type ContentType;
+        public byte[] content;
+
+        private Message(Type contentType, byte[] content)
+        {
+            ContentType = contentType;
+            this.content = content;
+        }
+
+        internal static Message Create(IPacket obj)
+        {
+            return new Message(obj.GetType(), Serializer.Serialize(obj));
+        }
+    }
+}
