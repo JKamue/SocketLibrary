a9eabec1161c46fbbd34bb20565d354bca081a9a
JKamue <mail@jkamue.de>
2020-10-25 15:26:41 +0100

    Added serialization and deserialization class

---
+++ SocketLibrary/Messages/Serializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace SocketLibrary.Messages
+{
+    internal static class Serializer
+    {
+        internal static byte[] Serialize<T>(T obj)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, obj);
+                return stream.ToArray();
+            }
+        }
+
+        internal static T Deserialize<T>(byte[] bytes)
+        {
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                IFormatter br = new BinaryFormatter();
+                return (T)br.Deserialize(ms);
+            }
+        }
+
+        internal static dynamic DeserializeType(byte[] bytes, Type type)
+        {
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                IFormatter br = new BinaryFormatter();
+                return Convert.ChangeType(br.Deserialize(ms), type);
+            }
+        }
+    }
+}
