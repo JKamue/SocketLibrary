df8bb6d79c04acf8494f3e59eca8ecb6ca485169
JKamue <mail@jkamue.de>
2020-10-25 15:31:18 +0100

    Added basic ISocketConnection implementation

---
+++ SocketLibrary/SocketConnection/UnsecuredUdpConnection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using SocketLibrary.Messages;
+
+namespace SocketLibrary.SocketConnection
+{
+    class UnsecuredUdpConnection : ISocketConnection
+    {
+        private readonly UdpClient _udpClient;
+        private readonly bool _debug;
+
+        public UnsecuredUdpConnection(int myPort, bool debug = false)
+        {
+            _udpClient = new UdpClient(myPort);
+            _debug = debug;
+            _udpClient.BeginReceive(DataReceived, _udpClient);
+        }
+
+        public event EventHandler<SocketEventArgs> OnMessageReceived;
+        public void Send(IPacket obj, IPEndPoint recipient)
+        {
+            var message = Message.Create(obj);
+            var content = Serializer.Serialize(message);
+            _udpClient.Send(content, content.Length, recipient);
+        }
+
+        private void DataReceived(IAsyncResult ar)
+        {
+            IPEndPoint receivedIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            try
+            {
+                var receivedBytes = _udpClient.EndReceive(ar, ref receivedIpEndPoint);
+                var message = Serializer.Deserialize<Message>(receivedBytes);
+                var content = Serializer.DeserializeType(message.content, message.ContentType);
+                OnMessageReceived?.Invoke(this, new SocketEventArgs(receivedIpEndPoint, content));
+            }
+            catch (SocketException e)
+            {
+            }
+            catch (ObjectDisposedException e)
+            {
+            }
+
+            _udpClient.BeginReceive(DataReceived, _udpClient);
+        }
+
+        public void Close()
+        {
+            _udpClient.Client?.Shutdown(SocketShutdown.Both);
+            _udpClient.Client?.Close();
+            _udpClient.Close();
+            _udpClient.Dispose();
+        }
+    }
+}
